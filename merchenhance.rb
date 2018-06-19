f11ddc039e950ca9f4f6485b4dacac74a5d8c90f
Can Toraman <cantoraman@gmail.com>
2018-06-19 14:13:48 +0100

    removes restock from items

--- merchenhance.rb
+++ merchenhance.rb
@@ -3,6 +3,7 @@ require( 'sinatra/contrib/all' )
 require_relative('controllers/items_controller')
 require_relative('controllers/manufacturers_controller')
 require_relative('controllers/transactions_controller')
+require_relative('controllers/order_controller')
 also_reload( './models/*' )
 
 
